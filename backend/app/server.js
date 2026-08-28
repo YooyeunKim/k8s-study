b7051d841382ddaaaa7aa93bbd55f2dfa11063e3
김유연 <kyyworld@hotmail.com>
2026-08-28 17:38:55 +0900

    initial Kubernetes study setup

---
+++ backend/app/server.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+const PORT = 5678;
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain; charset=utf-8'
+    });
+
+    res.end('Hello from My OpenStream Backend v2!\n');
+});
+
+server.listen(PORT, '0.0.0.0', () => {
+    console.log(`Backend server running on port ${PORT}`);
+});
